3232b72a80aa3af3cf82b440a4f38c528b710878
Elias <ilyasseessadak@gmail.com>
2024-01-28 19:45:00 +0100

    control de login

--- src/componentes/IniciarSesion.js
+++ src/componentes/IniciarSesion.js
@@ -1,33 +1,86 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 const IniciarSesion = () => {
+  const [nombreUsuario, setNombreUsuario] = useState("");
+  const [credencialUsuario, setCredencialUsuario] = useState("");
+
+  const manejarFormularioLogin = (e) => {
+    e.preventDefault();
+  
+    fetch(`http://localhost:8080/futbol-manager/usuario/${nombreUsuario}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        credencialUsuarioIntroducida: credencialUsuario,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("¡Error al iniciar sesión!");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.success) {
+          // Alerta de inicio de sesión exitoso
+          alert("Inicio de sesión exitoso");
+        } else {
+          // Alerta de inicio de sesión fallido
+          alert("Inicio de sesión fallido");
+        }
+      })
+      .catch((error) => console.error("Error al recuperar datos: ", error));
+  };
+  
+
+  const cerrarVentana = () => {};
+
   return (
     <div className="InicarSesion">
       <div className="card text-center ">
         <div className="card-header">Iniciar sesión</div>
         <div className="card-body">
-          <div className="row justify-content-center">
-            <div className="col col-3">
-              <div className="input-group mb-3">
-                <span className="input-group-text">Nombre</span>
-                <input type="text" className="form-control" />
+          <form onSubmit={manejarFormularioLogin}>
+            <div className="row justify-content-center">
+              <div className="col col-3">
+                <div className="input-group mb-3">
+                  <span className="input-group-text">Nombre</span>
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={nombreUsuario}
+                    onChange={(e) => setNombreUsuario(e.target.value)}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="row justify-content-center">
-            <div className="col col-3">
-              <div className="input-group mb-3">
-                <span className="input-group-text">Contraseña</span>
-                <input type="password" className="form-control" />
+            <div className="row justify-content-center">
+              <div className="col col-3">
+                <div className="input-group mb-3">
+                  <span className="input-group-text">Contraseña</span>
+                  <input
+                    type="password"
+                    className="form-control"
+                    value={credencialUsuario}
+                    onChange={(e) => setCredencialUsuario(e.target.value)}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-          <button type="submit" className="btn btn-primary me-3">
-            Iniciar sesión
-          </button>
-          <button type="submit" className="btn btn-danger">
-            Cancelar
-          </button>
+            <button type="submit" className="btn btn-primary me-3">
+              Iniciar sesión
+            </button>
+            <button
+              type="submit"
+              className="btn btn-danger"
+              onClick={cerrarVentana}
+            >
+              Cancelar
+            </button>
+          </form>
         </div>
       </div>
     </div>
